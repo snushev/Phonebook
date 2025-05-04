1da41354a6f34a10b9e9b120e15459a2d1eb98d5
Simeon Nushev <s.nushev@gmail.com>
2025-05-04 16:42:11 +0300

    Delete djangoproject directory

--- djangoproject/djangoproject/phonebook/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from djangoproject.phonebook.views import landing_page, create_contact, delete_contact
-
-urlpatterns = [
-    path('', landing_page, name='landing_page'),
-    path('new-contact', create_contact, name='new-contact'),
-    path('delete/<int:contact_id>', delete_contact, name='delete')
-]
